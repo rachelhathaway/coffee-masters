be8223ec2d4b2fd209c8a15fb9c6131f2a9e031d
Rachel Hathaway <rachel.e.hathaway@gmail.com>
2023-08-18 17:26:13 -0500

    feat: add router service

---
+++ src/services/router.ts
@@ -0,0 +1,20 @@
+import type { Services } from "../types";
+
+export const router: Services["Router"] = {
+  initialize: () => {
+    const navItems = document.querySelectorAll<HTMLAnchorElement>("a.navlink");
+
+    navItems?.forEach((navItem) =>
+      navItem.addEventListener("click", function handleClick(event) {
+        event.preventDefault();
+        router.navigateTo(this.href);
+      })
+    );
+
+    if (location.pathname !== "/") {
+      router.navigateTo(location.pathname);
+    }
+  },
+  navigateTo: (url: string, state?: Record<string, any>) =>
+    history.pushState(state, "", url),
+};
