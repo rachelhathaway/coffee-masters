572c266e3a65425958c6e8faa6a75c170c4b5e6e
Rachel Hathaway <rachel.e.hathaway@gmail.com>
2023-08-19 08:49:42 -0500

    feat: add basic routing functionality

--- src/services/router.ts
+++ src/services/router.ts
@@ -1,20 +1,71 @@
 import type { Services } from "../types";
 
+const initializeNav = () => {
+  const navItems = document.querySelectorAll<HTMLAnchorElement>("a.navlink");
+
+  navItems?.forEach((navItem) =>
+    navItem.addEventListener("click", function handleClick(event) {
+      event.preventDefault();
+
+      const href = navItem.getAttribute("href");
+
+      if (href) {
+        router.navigateTo(href);
+      }
+    })
+  );
+
+  if (location.pathname !== "/") {
+    router.navigateTo(location.pathname);
+  }
+};
+
+const setPageContent = (url: string) => {
+  let newPageEl = document.createElement("h1");
+
+  switch (url) {
+    case "/": {
+      newPageEl.textContent = "Menu";
+      break;
+    }
+    case "/order": {
+      newPageEl.textContent = "Your Order";
+      break;
+    }
+    default:
+      newPageEl.textContent = "404";
+      break;
+  }
+
+  const mainEl = document.querySelector("main");
+  const prevPageEl = mainEl?.firstChild;
+
+  if (prevPageEl) {
+    mainEl.replaceChild(newPageEl, prevPageEl);
+  } else {
+    mainEl?.appendChild(newPageEl);
+  }
+};
+
+const resetScrollPosition = () => {
+  window.scrollX = 0;
+  window.scrollY = 0;
+};
+
 export const router: Services["Router"] = {
   initialize: () => {
-    const navItems = document.querySelectorAll<HTMLAnchorElement>("a.navlink");
-
-    navItems?.forEach((navItem) =>
-      navItem.addEventListener("click", function handleClick(event) {
-        event.preventDefault();
-        router.navigateTo(navItem.href);
-      })
-    );
-
-    if (location.pathname !== "/") {
-      router.navigateTo(location.pathname);
-    }
+    initializeNav();
   },
-  navigateTo: (url: string, state?: Record<string, any>) =>
-    history.pushState(state, "", url),
+  navigateTo: (
+    url: string,
+    state?: Record<string, any>,
+    addToHistory = true
+  ) => {
+    if (addToHistory) {
+      history.pushState(state, "", url);
+    }
+
+    setPageContent(url);
+    resetScrollPosition();
+  },
 };
