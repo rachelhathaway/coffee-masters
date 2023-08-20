fa334777b66700465e05a8740d55813e57148189
Rachel Hathaway <rachel.e.hathaway@gmail.com>
2023-08-20 08:04:02 -0500

    chore: add constants

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+export const EVENTS = {
+  appCartChange: "appcartchange",
+  appMenuChange: "appmenuchange",
+};
+
+export const TEMPLATE_IDS = {
+  detailsPage: "details-page-template",
+  menuPage: "menu-page-template",
+  orderPage: "order-form-template",
+};
