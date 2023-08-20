9d0d46efdece8b30a83a7ddcfd72e402395eba7c
Rachel Hathaway <rachel.e.hathaway@gmail.com>
2023-08-20 09:58:28 -0500

    chore: render product item component

--- src/components/MenuPage.ts
+++ src/components/MenuPage.ts
@@ -1,10 +1,56 @@
-import { TEMPLATE_IDS } from "../constants";
+import { EVENTS, TEMPLATE_IDS } from "../constants";
+import { storeProxy } from "../services/store";
 import css from "./MenuPage.css?inline";
 import { BasePageComponent } from "./BasePageComponent";
 
 export class MenuPage extends BasePageComponent {
+  #rootSelector: Element | null = null;
+
   constructor() {
     super(TEMPLATE_IDS.menuPage, css);
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    this.#rootSelector = this.getRoot().querySelector("#menu");
+
+    window.addEventListener(EVENTS.appMenuChange, () => {
+      this.render();
+    });
+
+    this.render();
+  }
+
+  render() {
+    if (!this.#rootSelector) return;
+
+    this.#rootSelector.innerHTML = "";
+
+    if (storeProxy.menu.length) {
+      for (const category of storeProxy.menu) {
+        const categoryEl = document.createElement("li");
+
+        categoryEl.innerHTML = `
+          <h3>${category.name}</h3>
+          <ul class="category"></ul>
+        `;
+
+        for (const product of category.products) {
+          const productEl = document.createElement("li");
+          const productItemEl = document.createElement("cm-product-item");
+
+          productItemEl.dataset.product = JSON.stringify(product);
+          productEl.appendChild(productItemEl);
+
+          categoryEl.querySelector("ul")?.appendChild(productEl);
+        }
+
+        this.#rootSelector.appendChild(categoryEl);
+      }
+    } else {
+      this.#rootSelector.innerHTML = "Loading...";
+    }
   }
 }
 
